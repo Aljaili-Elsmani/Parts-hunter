ab6f6ec3ce6fabc81af757077589fb8461537ed2
Aljaili-Elsmani <jailanismani@gmail.com>
2025-07-17 14:50:20 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render يحتاج استخدام host='0.0.0.0' و port من متغير البيئة
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
