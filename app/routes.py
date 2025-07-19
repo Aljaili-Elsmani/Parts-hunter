ec05e414855b37f4b97605168a0e8f6000d0cfc2
Aljaili-Elsmani <jailanismani@gmail.com>
2025-07-19 19:56:14 +0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,22 @@
 from flask import render_template, request
 from app import app
 
+# الصفحة الرئيسية
 @app.route('/')
 def home():
     return render_template("index.html")
 
+# صفحة "عن الموقع"
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# صفحة "تواصل معنا"
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+# صفحة "طلب قطع غيار"
 @app.route('/order', methods=["GET", "POST"])
 def order():
     if request.method == "POST":
