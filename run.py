d8f2eac734889ae621e4754ea2e5982603726e44
Aljaili-Elsmani <jailanismani@gmail.com>
2025-11-27 22:01:22 +0200

    Simplify app.run() configuration
    Removed host and port parameters from app.run()

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    # debug=True لتسهيل التطوير، في الإنتاج يمكن تغييره إلى False
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
