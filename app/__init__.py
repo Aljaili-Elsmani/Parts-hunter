e33067a528d9cbb38b95fba3e2d93a1c2f3a1ef3
Aljaili-Elsmani <jailanismani@gmail.com>
2025-11-28 03:53:58 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,27 @@ from flask import Flask
 import sqlite3
 import os
 
-app = Flask(__name__)
-app.secret_key = "your_secret_key"
+DB_NAME = "database.db"
 
-DB_NAME = "/opt/render/project/src/database.db"
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "your_secret_key"
 
-# إنشاء قاعدة البيانات إذا لم تكن موجودة
-if not os.path.exists(DB_NAME):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            price TEXT NOT NULL,
-            category TEXT NOT NULL
-        )
-    """)
-    conn.commit()
-    conn.close()
+    # إنشاء قاعدة البيانات والجدول إذا لم يكن موجودًا
+    if not os.path.exists(DB_NAME):
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute("""
+            CREATE TABLE products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                price TEXT NOT NULL,
+                category TEXT NOT NULL
+            )
+        """)
+        conn.commit()
+        conn.close()
 
-from app import routes
+    # استدعاء جميع الروابط (routes)
+    from app import routes
+    return app
