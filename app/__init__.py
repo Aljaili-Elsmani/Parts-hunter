c27c92340b9be401c9ca889831f4f696bbb166ce
Aljaili-Elsmani <jailanismani@gmail.com>
2025-11-28 03:41:13 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
-import os
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
-DB_NAME = os.path.join(os.path.dirname(__file__), "..", "database.db")
+DB_NAME = "/opt/render/project/src/database.db"
 
 # إنشاء قاعدة البيانات إذا لم تكن موجودة
 if not os.path.exists(DB_NAME):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute("""
-        CREATE TABLE products (
+        CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             price TEXT NOT NULL,
